09a70486d0af0505c2e215773c2e45f35d20b4dc
Jarrian <89616705+godkingjay@users.noreply.github.com>
2022-09-04 15:29:51 +0800

    update site

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ const shareBtn = document.querySelector('.share-btn');
 
 shareBtn.addEventListener('click', () => {
   shareBtn.classList.toggle('active-btn');
+  const sharePopup = document.querySelector('.share-pop-up');
+  sharePopup.classList.toggle('share-pop-up-show');
 });
